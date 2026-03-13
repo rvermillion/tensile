9dd40daab670141dc02c70196a0d4a7c6db83246
Richard Vermillion <rvermillion@fulcrumanalytics.com>
2026-03-13 17:58:46 -0400

    Enhanced the instrument class to have better providers, to do better composition, and to allow you to remove them easily.

--- python/tensile/nn/module/instrument.py
+++ python/tensile/nn/module/instrument.py
@@ -1,6 +1,6 @@
 #  Copyright (c) 2026. Richard Vermillion. All Rights Reserved.
 
-from ..common import ten, Object, field, Annotated, Callable, Optional, Protocol, TypeVar, TYPE_CHECKING
+from ..common import *
 
 if TYPE_CHECKING:
     import tensile.nn.module
@@ -28,17 +28,56 @@ class Instrument(Object):
     def wrap_call(self, module: 'tensile.nn.module.Module', call: C, training: bool) -> C:
         return call
 
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Instrument):
+            return self._eq_tuple() == other._eq_tuple()
+        return False
+
+    def __hash__(self):
+        return hash(self._eq_tuple())
+
+    def _eq_tuple(self) -> tuple:
+        return self.__class__, self.wrap_call
+
+    def __add__(self, other) -> 'Instrument':
+        if other is None: return self
+        return Instrument.compose(self, Instrument.coerce(other))
+
+    def __radd__(self, other) -> 'Instrument':
+        if other is None: return self
+        return Instrument.compose(Instrument.coerce(other), self)
+
+    def __sub__(self, other: Union['Instrument', type]) -> Optional['Instrument']:
+        if isinstance(other, Instrument):
+            return self.remove(predicates.eq(other))
+        if isinstance(other, type):
+            return self.remove(predicates.is_instance(other))
+        return self
+
+    def remove(self, where: Predicate['Instrument'] = None) -> Optional['Instrument']:
+        if where is None or where(self): return None
+        return self
+
     @classmethod
     def compose(cls, a: Optional['Instrument'], b: Optional['Instrument']) -> 'Instrument':
         if a is None: return b
         if b is None: return a
         ai = a.instruments if isinstance(a, CompositeInstrument) else [a]
         bi = b.instruments if isinstance(b, CompositeInstrument) else [b]
-        return CompositeInstrument(instruments=ai+bi)
+        return CompositeInstrument(instruments=tuple(ai+bi))
 
     @classmethod
     def _coerce_from_callable(cls, spec: Callable, /, **kwargs):
         return cls.from_wrapper(spec)
+
+    @classmethod
+    def _coerce_from_sequence(cls, spec: Sequence, /, **kwargs):
+        instruments = tuple(Instrument.coerce(i) for i in spec if i is not None)
+        if instruments:
+            if len(instruments) == 1:
+                return instruments[0]
+            return CompositeInstrument(instruments=instruments)
+        raise ValueError(f"Expected a sequence of instruments: {spec}")
 
     @classmethod
     def from_wrapper(cls, wrap_call: ModuleCallWrapper):
@@ -57,12 +96,15 @@ class WrapperInstrument(Instrument):
     def _repr_args(self, **options) -> str:
         return self.wrap_call.__name__
 
+    def _eq_tuple(self) -> tuple:
+        return self.wrap_call,
+
 
 class CompositeInstrument(Instrument):
 
     __slots__ = ('instruments',)
 
-    instruments: Annotated[list[Instrument], field(
+    instruments: Annotated[tuple[Instrument, ...], field(
         doc='List of instruments to apply',
         required=True,
     )]
@@ -73,3 +115,16 @@ class CompositeInstrument(Instrument):
             wrapped = instrument.wrap_call(module, wrapped, training)
         return wrapped
 
+    def remove(self, where: Predicate['Instrument'] = None) -> Optional['Instrument']:
+        if where is None or where(self): return None
+        instruments = [inst.remove(where) for inst in self.instruments]
+        if instruments == self.instruments:
+            return self
+        instruments = tuple(inst for inst in instruments if inst is not None)
+        if instruments:
+            if len(instruments) == 1: return instruments[0]
+            return CompositeInstrument(instruments=instruments)
+        return None
+
+    def _eq_tuple(self) -> tuple:
+        return self.instruments
