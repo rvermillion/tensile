59e15337c745329259feb2e9e7b88fc44074ace4
Richard Vermillion <rvermillion@fulcrumanalytics.com>
2026-03-13 14:32:52 -0400

    Added the `tensile.extra` package for non-core, but usefule, extensions.

---
+++ python/tensile/extra/__init__.py
@@ -0,0 +1,6 @@
+#  Copyright (c) 2026. Richard Vermillion. All Rights Reserved.
+
+"""The `tensile.extra` package contains non-core, but potentially useful extensions
+   of the core tensile module, layer, loss and instrument classes.
+
+"""
